a483e05d7ee99c9d825cf9e75a713d28bef61038
GPCWGR <iseeonlyanimation@gmail.com>
2026-05-23 22:32:14 -0700

    RaknetSupported.lua

--- RaknetOnly.lua
+++ RaknetOnly.lua
@@ -1,7 +1,13 @@
 r.net:SendRaw(1)
 hook r.net if value = "1"
 then
-r.net:ConvertBytes(value)
+r.net:ConvertByte(value)
+  if value then
+    print(value)
+    r.net:SetScriptSource:value/Lua
+    index.spoof("lua code here")
 print(value)
+run:index.spoof()
+  end
 end
 end
